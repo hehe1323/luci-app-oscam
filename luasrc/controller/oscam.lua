c0dcb0a2b63da1cb75b51543e50a807194b23d19
hehe1323 <chirongda@hotmail.com>
2025-02-02 08:46:23 +0800

    Update oscam.lua

--- luasrc/controller/oscam.lua
+++ luasrc/controller/oscam.lua
@@ -9,6 +9,14 @@ function index()
     entry({"admin", "services", "oscam", "action"}, call("action")).leaf = true
 end
 
+
+function stream_log()
+-- 在 oscam.lua 控制器中添加
+    entry({"admin", "services", "oscam", "log"}, call("stream_log"))    
+    local t = require "luci.template"
+    t.render("oscam/logs")
+end
+
 function action()
     local cmd = luci.http.formvalue("cmd")
     if cmd == "start" then
