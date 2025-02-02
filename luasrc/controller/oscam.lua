fcfd7b5c1c46b4d8ff6222757f39e08527f294c0
hehe1323 <chirongda@hotmail.com>
2025-02-02 10:58:18 +0800

    Update oscam.lua

--- luasrc/controller/oscam.lua
+++ luasrc/controller/oscam.lua
@@ -1,31 +1,17 @@
 module("luci.controller.oscam", package.seeall)
 
 function index()
-    if not nixio.fs.access("/etc/config/oscam") then
-        return
-    end
-
-    entry({"admin", "services", "oscam"}, cbi("oscam"), _("OSCam"), 60).dependent = true
-    entry({"admin", "services", "oscam", "action"}, call("action")).leaf = true
+    entry({"admin", "services", "oscam"}, firstchild(), _("OSCam"), 60).dependent = false
+    entry({"admin", "services", "oscam", "status"}, call("action_status"), _("Status"), 10)
+    entry({"admin", "services", "oscam", "config"}, cbi("oscam"), _("Configuration"), 20)
 end
 
-
-function stream_log()
--- 在 oscam.lua 控制器中添加
-    entry({"admin", "services", "oscam", "log"}, call("stream_log"))    
-    local t = require "luci.template"
-    t.render("oscam/logs")
-end
-
-function action()
-    local cmd = luci.http.formvalue("cmd")
-    if cmd == "start" then
-        luci.sys.init.start("oscam")
-    elseif cmd == "stop" then
-        luci.sys.init.stop("oscam")
-    elseif cmd == "restart" then
-        luci.sys.init.restart("oscam")
-    end
+function action_status()
+    local sys = require "luci.sys"
+    local status = {
+        running = (sys.call("pgrep oscam >/dev/null") == 0),
+        pcsc_enabled = (luci.model.uci.cursor():get("oscam", "main", "pcsc") or "0") == "1"
+    }
     luci.http.prepare_content("application/json")
-    luci.http.write_json({ status = "OK" })
+    luci.http.write_json(status)
 end
